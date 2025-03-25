a808984cf2d168a9837778b431c0abbab7ac8746
jscheffner <jscheffner@users.noreply.github.com>
2025-03-25 22:00:08 +0100

    replace tslint with eslint and neostandard
    tslint is no longer maintained

--- src/extension.ts
+++ src/extension.ts
@@ -1,61 +1,61 @@
-import { window, workspace, ExtensionContext, commands, TextEditor, Terminal, WorkspaceFolder } from 'vscode';
+import { window, workspace, ExtensionContext, commands, TextEditor, Terminal } from 'vscode'
 
 const getActiveWorkspaceTerminal = (editor: TextEditor, terminals: readonly Terminal[]): Terminal | undefined => {
-  const folder = workspace.getWorkspaceFolder(editor.document.uri);
+  const folder = workspace.getWorkspaceFolder(editor.document.uri)
   if (!folder) {
-    return;
+    return
   }
 
-  return terminals.find(({ name }) => (name === folder.name));
-};
+  return terminals.find(({ name }) => (name === folder.name))
+}
 
 const openWorkspaceTerminals = () => {
-  const { workspaceFolders = [] } = workspace;
-  const { activeTextEditor } = window;
+  const { workspaceFolders = [] } = workspace
+  const { activeTextEditor } = window
 
   const createdTerminals = workspaceFolders
     .filter(({ name }) => !window.terminals.find(term => term.name === name))
-    .map(({ uri, name }) => window.createTerminal({ cwd: uri, name }));
+    .map(({ uri, name }) => window.createTerminal({ cwd: uri, name }))
 
-  const activeWorkspaceTerminal = activeTextEditor && getActiveWorkspaceTerminal(activeTextEditor, createdTerminals);
-  const switchTo = activeWorkspaceTerminal || createdTerminals[0];
+  const activeWorkspaceTerminal = activeTextEditor && getActiveWorkspaceTerminal(activeTextEditor, createdTerminals)
+  const switchTo = activeWorkspaceTerminal || createdTerminals[0]
 
-  switchTo?.show(true);
-};
+  switchTo?.show(true)
+}
 
 const autoExecute = () => {
-  const { auto } = workspace.getConfiguration('workspace-terminals');
+  const { auto } = workspace.getConfiguration('workspace-terminals')
   const numberOfWorkspaceFolders = workspace.workspaceFolders
     ? workspace.workspaceFolders.length
-    : 0;
+    : 0
 
   if (auto === 'always' || (auto === 'multiRoot' && numberOfWorkspaceFolders > 1)) {
-    openWorkspaceTerminals();
+    openWorkspaceTerminals()
   }
-};
+}
 
 const switchTerminal = (editor: TextEditor | undefined) => {
-  const { workspaceFolders, getConfiguration } = workspace;
-  const { switchTerminal } = getConfiguration('workspace-terminals');
-  const { activeTerminal } = window;
+  const { workspaceFolders, getConfiguration } = workspace
+  const { switchTerminal } = getConfiguration('workspace-terminals')
+  const { activeTerminal } = window
 
   const isActiveTerminalWorkspaceTerminal = () => activeTerminal
     && workspaceFolders
-    && workspaceFolders.some(({ name }) => name === activeTerminal.name);
+    && workspaceFolders.some(({ name }) => name === activeTerminal.name)
 
   if (!editor || switchTerminal === 'never' || (switchTerminal === 'fromWorkspaceTerminals' && !isActiveTerminalWorkspaceTerminal())) {
-    return;
+    return
   }
 
-  getActiveWorkspaceTerminal(editor, window.terminals)?.show(true);
-};
-
-export function activate(context: ExtensionContext) {
-  commands.registerCommand('extension.openWorkspaceTerminals', openWorkspaceTerminals);
-  autoExecute();
-  workspace.onDidChangeConfiguration(autoExecute);
-  workspace.onDidChangeWorkspaceFolders(autoExecute);
-  window.onDidChangeActiveTextEditor(switchTerminal);
+  getActiveWorkspaceTerminal(editor, window.terminals)?.show(true)
 }
 
-export function deactivate() { }
+export function activate (context: ExtensionContext) {
+  commands.registerCommand('extension.openWorkspaceTerminals', openWorkspaceTerminals)
+  autoExecute()
+  workspace.onDidChangeConfiguration(autoExecute)
+  workspace.onDidChangeWorkspaceFolders(autoExecute)
+  window.onDidChangeActiveTextEditor(switchTerminal)
+}
+
+export function deactivate () { }
